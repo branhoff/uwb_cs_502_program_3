d039ce624635a64b09e1c3e1f9c3c66be6f4715e
branhoff <branhoff2@gmail.com>
2023-02-11 10:15:41 -0800

    refactoring braces

--- Graph.cpp
+++ Graph.cpp
@@ -18,18 +18,14 @@ using namespace std;
 //    - T[i][j].path set to -1 for all i and j
 //    - C[i][j] set to 0 for all i and j
 //    - size set to 0
-Graph::Graph()
-{
-   for (int v = 1; v < MAX_VERTICES; v++)
-   {
+Graph::Graph() {
+   for (int v = 1; v < MAX_VERTICES; v++) {
       vertices[v].data = nullptr;
       vertices[v].edgeHead = nullptr;
    }
 
-   for (int i = 0; i < MAX_VERTICES; i++)
-   {
-      for (int j = 0; j < MAX_VERTICES; j++)
-      {
+   for (int i = 0; i < MAX_VERTICES; i++) {
+      for (int j = 0; j < MAX_VERTICES; j++) {
          T[i][j].dist = INT_MAX;
          T[i][j].visited = false;
          T[i][j].path = -1;
@@ -43,8 +39,7 @@ Graph::Graph()
 // Constructs a copy of a graph object
 // Preconditions: The graph object g is initialized
 // Postconditions: A new graph object is created with the same values as the original graph object g
-Graph::Graph(const Graph& g) : Graph()
-{
+Graph::Graph(const Graph& g) : Graph() {
    copy(g);
 }
 
@@ -52,8 +47,7 @@ Graph::Graph(const Graph& g) : Graph()
 // Destructor for the Graph class
 // Preconditions:  Graph object has been created
 // Postconditions: Graph object's memory is deallocated and its resources are freed
-Graph::~Graph()
-{
+Graph::~Graph() {
    clear();
 }
 
@@ -62,10 +56,10 @@ Graph::~Graph()
 // Preconditions:  The graph object must be properly initialized.
 // Postconditions: The graph object is copied from the input graph object, including
 //                 all vertices and edges. The previous data in the graph object is deleted.
-Graph& Graph::operator=(const Graph& g)
-{
-   if (this == &g) // check self-assignment
+Graph& Graph::operator=(const Graph& g) {
+   if (this == &g) { // check self-assignment
       return *this;
+   }
 
    clear();
    copy(g);
@@ -106,7 +100,6 @@ void Graph::buildGraph(ifstream& infile) {
 // Prints the vertices in the graph
 // Preconditions:  Graph object is initialized with vertices
 // Postconditions: The description of each vertex in the graph is printed to the console
-
 void Graph::printVertices() {
    for (int i = 1; i <= size; i++) {
       cout << "Vertex " << i << ": " << vertices[i].data->getDescription() << endl;
@@ -168,35 +161,26 @@ void Graph::printEdges() {
 // Precondition: The graph must be initialized with vertices and edges.
 // Postcondition: The shortest path is stored in a 2D table T[MAX_VERTICES][MAX_VERTICES], 
 //                where T[i][j] represents the cost of the shortest path from vertex i to vertex j.
-void Graph::findShortestPath()
-{
-   for (int i = 1; i <= size; i++)
-   {
-      for (int j = 1; j <= size; j++)
-      {
+void Graph::findShortestPath() {
+   for (int i = 1; i <= size; i++) {
+      for (int j = 1; j <= size; j++) {
          T[i][j].dist = INT_MAX;
          T[i][j].visited = false;
          T[i][j].path = -1;        
       }
 
       T[i][i].dist = 0;
-      
 
       int v = 0;  // smallest vertex
 
-      while (true) 
-      {
+      while (true) {
          v = -1;
-
          int min_dist = INT_MAX;
          
           // pick the vertex with the smallest distance in visited node
-         for (int j = 1; j <= size; j++)
-         {
-            if (T[i][j].visited == false )
-            {
-               if (T[i][j].dist < min_dist)
-               {
+         for (int j = 1; j <= size; j++) {
+            if (T[i][j].visited == false ) {
+               if (T[i][j].dist < min_dist) {
                   min_dist = T[i][j].dist;
                   v = j;
                }
@@ -207,56 +191,53 @@ void Graph::findShortestPath()
             break;
          }
 
-         
          T[i][v].visited = true;
 
          // iterate the adjus
          VertexNode node = vertices[v];
          EdgeNode* curr = node.edgeHead;
 
-         while (curr != nullptr)
-         {
+         while (curr != nullptr) {
             int u = curr->adjVertex;
             int weight = curr->weight;
 
-            if (T[i][v].dist + weight < T[i][u].dist && !T[i][u].visited)
-            {
+            if (T[i][v].dist + weight < T[i][u].dist && !T[i][u].visited) {
                T[i][u].dist = T[i][v].dist + weight;
                T[i][u].path = v;               
             }            
-
             curr = curr->nextEdge;
          }
-
       }
-     
    }
 }
 
+//------------------------------- displayAll -------------------------------
+// Displays the shortest paths between all vertices in the graph
+// Preconditions:  The graph is not empty and the T matrix has been populated
+//                 with the shortest path data
+// Postconditions: The shortest paths between all vertices in the graph have
+//                 been displayed on the console
 void Graph::displayAll() {
    cout << "Shortest paths between all vertices:" << endl;
    cout << setw(30) << left << "Description" << setw(6) << left << "From" << setw(6) << left << "To" << setw(6) << left << "Dist" << "Path" << endl;
    for (int i = 1; i <= size; i++) {
       cout << vertices[i].data->getDescription() << endl;
-      for (int j = 1; j <= size; j++)
-      {
-         if (i == j)
+      for (int j = 1; j <= size; j++) {
+         if (i == j) {
             continue;
+         }
 
          cout << setw(30) << left << "" << setw(6) << left << i << setw(6) << left << j;
-         if (T[i][j].visited)
-         {
+         if (T[i][j].visited) {
             cout << setw(6) << left << T[i][j].dist;
             // generate path
             string path = calcPath(i, j);
           
             cout << path;
          }
-         else
-         {
+         else {
             cout << setw(6) << left << "--";
          }
-
          cout << endl;
       }    
    }
@@ -272,11 +253,9 @@ void Graph::displayAll() {
 // Postconditions: The shortest path from the source vertex to the destination
 //                 vertex, including the total cost and the list of vertices
 //                 visited along the way, is displayed on the console.
-void Graph::display(int src, int dst)
-{
+void Graph::display(int src, int dst) {
    cout << setw(6) << left << src << setw(6) << left << dst;
-   if (T[src][dst].visited)
-   {
+   if (T[src][dst].visited) {
       cout << setw(6) << left << T[src][dst].dist;
 
       string path = calcPath(src, dst);
@@ -287,8 +266,7 @@ void Graph::display(int src, int dst)
       cout << visited_vertices;
       cout << endl;
    }
-   else
-   {
+   else {
       cout << setw(6) << left << "--";
       cout << endl;
    }
@@ -300,10 +278,10 @@ void Graph::display(int src, int dst)
 //                `findShortestPath` method must have been executed.
 // Postconditions: Returns a string representation of the path from the source vertex 
 //                 to the destination vertex, represented as a sequence of vertex IDs separated by spaces.
-string Graph::calcPath(int src, int dst)
-{
-   if (T[src][dst].path < 0)
+string Graph::calcPath(int src, int dst) {
+   if (T[src][dst].path < 0) {
       return to_string(dst);
+   }
 
    return calcPath(src, T[src][dst].path) + " " + to_string(dst);
 }
@@ -316,11 +294,11 @@ string Graph::calcPath(int src, int dst)
 // Postconditions: The method returns the description of the shortest path from 
 //                 the source vertex to the destination vertex.
 
-string Graph::getVerticesName(int src, int dst)
-{
+string Graph::getVerticesName(int src, int dst) {
    string desc = vertices[dst].data->getDescription();
-   if (T[src][dst].path < 0)
+   if (T[src][dst].path < 0) {
       return desc;
+   }
 
    return getVerticesName(src, T[src][dst].path) + "\n" + desc;
 }
@@ -329,22 +307,17 @@ string Graph::getVerticesName(int src, int dst)
 // Clears the graph of all vertices and edges
 // Preconditions:  The graph object must be initialized
 // Postconditions: The graph object will be cleared of all vertices and edges, and its size will be reset to 0. All dynamically allocated memory will be freed.
-void Graph::clear()
-{
-   for (int v = 1; v <= size; v++)
-   {
-      if (vertices[v].data != nullptr)
-      {
+void Graph::clear() {
+   for (int v = 1; v <= size; v++) {
+      if (vertices[v].data != nullptr) {
          delete vertices[v].data;
          vertices[v].data = nullptr;
       }
 
-      if (vertices[v].edgeHead != nullptr)
-      {
+      if (vertices[v].edgeHead != nullptr) {
          EdgeNode* curr = vertices[v].edgeHead;
 
-         while (curr != nullptr)
-         {
+         while (curr != nullptr) {
             EdgeNode* next = curr->nextEdge;
             delete curr;
             curr = next;
@@ -360,39 +333,32 @@ void Graph::clear()
 // Copies the data from a given Graph object into the current Graph object.
 // Preconditions: The input Graph object must be properly initialized with vertices and edges.
 // Postconditions: The current Graph object is initialized with the same vertices and edges as the input Graph object.
-void Graph::copy(const Graph& g)
-{
+void Graph::copy(const Graph& g) {
    // copy vertices data
    size = g.size;
-   for (int v = 1; v <= g.size; v++)
-   {
-      if (g.vertices[v].data != nullptr)
-      {
+   for (int v = 1; v <= g.size; v++) {
+      if (g.vertices[v].data != nullptr) {
          vertices[v].data = new Vertex(g.vertices[v].data->getDescription());
          vertices[v].data->setCost(g.vertices[v].data->getCost());
       }
    }
 
    // copy edges data
-   for (int v = 1; v < g.size; v++)
-   {
+   for (int v = 1; v < g.size; v++) {
       EdgeNode* currg = g.vertices[v].edgeHead;
       EdgeNode* curr = nullptr;
 
-      while (currg != nullptr)
-      {
+      while (currg != nullptr) {
          EdgeNode* newEdge = new EdgeNode;
          newEdge->adjVertex = currg->adjVertex;
          newEdge->weight = currg->weight;
          newEdge->nextEdge = nullptr;
 
-         if (curr == nullptr)
-         {
+         if (curr == nullptr) {
             // create head
             vertices[v].edgeHead = newEdge;
          }
-         else
-         {
+         else {
             curr->nextEdge = newEdge;
          }
          curr = newEdge;
